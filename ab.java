df433c0ce138c081932d5f06fb7c06ffa5ebbf02
DivyaSowmiya <divya.vcse@ksrce.ac.in>
2016-07-21 17:20:16 +0530

    Create ab.java

---
+++ ab.java
@@ -0,0 +1,13 @@
+
+public class ab {
+	public static void leftshift(int i, int j)
+	{
+	 i<<;}
+	public static void main(String[] args) {
+		int i = 4, j = 2;
+		leftshift(i, j);
+		System.out.println(i);
+	
+	}
+
+}

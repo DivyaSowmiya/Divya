78f08d2f3e3dc58955996c1197313a01aab152f0
DivyaSowmiya <divya.vcse@ksrce.ac.in>
2016-07-21 17:23:54 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		int n;
+		int a = 0;
+		int b;
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the nuber");
+		n=s.nextInt();
+		int len=Integer.toString(n).length();
+		for(int i=0;i<len;i++)
+		{
+			for(int j=0;j<len;j++)
+			{
+				
+			}
+		
+		}
+	}
+
+}
